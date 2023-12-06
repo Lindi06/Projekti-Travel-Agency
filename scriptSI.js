58f987a2a7e13f85081374ec2ff90bd4d4b32624
dor1kk <dorajetkukaj@gmail.com>
2023-12-06 15:00:41 +0200

    mesazhi

--- scriptSI.js
+++ scriptSI.js
@@ -1,6 +1,25 @@
 
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+
+
+
+
 let emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
-    let passwordRegex = /(?=^.{8,}$)((?=.\d)|(?=.\W+))(?![.\n])(?=.[A-Z])(?=.[a-z]).*$/;
+let passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/;
+let nameRegex = /^[a-zA-Z]+$/;
+
+
  
 
 function validateForm(){
@@ -23,4 +42,32 @@ function validateForm(){
 
         alert('Forma është dorëzuar me sukses!');
       }
+    }
+
+
+    function validateSignUp(){
+        let name=document.getElementById('sign-up-name').value;
+        let email = document.getElementById('sign-up-email').value;
+    let password = document.getElementById('sign-up-password').value;
+
+    if(!nameRegex.test(name)){
+        alert("name invalid")
+        return;
+    }
+
+    if (!emailRegex.test(email)) {
+        alert('email invalid')
+        return;
+    }
+
+    if (!passwordRegex.test(password)) {
+        alert('password invalid')
+        return;
+    }
+
+    else{
+
+    alert('Jeni regjistruar me sukses');
+  }
+
     }
