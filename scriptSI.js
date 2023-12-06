a1daa06b7d19f41650bafeaccdb602d7ac9f4124
Lindi06 <rd68596@ubt-uni.net>
2023-12-06 13:43:25 +0100

    part6

--- scriptSI.js
+++ scriptSI.js
@@ -1,34 +1,26 @@
 
-let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-let passwordRegex = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+let emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+    let passwordRegex = /(?=^.{8,}$)((?=.\d)|(?=.\W+))(?![.\n])(?=.[A-Z])(?=.[a-z]).*$/;
+ 
 
-    
 function validateForm(){
 
 
-let email = document.getElementById('sign-in-email').value;
-let password = document.getElementById('sign-in-password').value;
+    let email = document.getElementById('sign-in-email').value;
+    let password = document.getElementById('sign-in-password').value;
 
-    if (!emailRegex.test(email)) {
-        alert('email invalid')
-        return;
+        if (!emailRegex.test(email)) {
+            alert('email invalid')
+            return;
+        }
+
+        if (!passwordRegex.test(password)) {
+            alert('password invalid')
+            return;
+        }
+
+        else{
+
+        alert('Forma është dorëzuar me sukses!');
+      }
     }
-
-    if (!passwordRegex.test(password)) {
-        alert('password invalid')
-        return;
-    }
-
-    else{
-
-    alert('Forma është dorëzuar me sukses!');
-  }
-}
-
-
-<<<<<<< HEAD
-=======
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
->>>>>>> 06da2a29ec5ed53245f8c7f0c04ed3e89f707fa7
