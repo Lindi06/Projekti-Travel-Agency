6d16d5d780e6cb5607b0ac5e368321ff6bca009d
Lindi06 <rd68596@ubt-uni.net>
2024-01-22 20:12:46 +0100

    test

---
+++ model/ticket.php
@@ -0,0 +1,53 @@
+<?php
+
+
+class Ticket {
+    private $Id;
+    private $departure_date;
+    private $arrival_date;
+    private $number_tickets;
+    private $destination_id;
+
+    public function __construct($departure_date, $arrival_date, $number_tickets, $destination_id) {
+        $this->departure_date = $departure_date;
+        $this->arrival_date = $arrival_date;
+        $this->number_tickets = $number_tickets;
+        $this->destination_id = $destination_id;
+    }
+
+    public function getDepartureDate() {
+        return $this->departure_date;
+    }
+
+    public function setDepartureDate($d) {
+        $this->departure_date = $d;
+    }
+
+    public function getArrivalDate() {
+        return $this->arrival_date;
+    }
+
+    public function setArrivalDate($a) {
+        $this->arrival_date = $a;
+    }
+
+    public function getNumberOfTickets() {
+        return $this->number_tickets;
+    }
+
+    public function setNumberOfTickets($nt) {
+        $this->number_tickets = $nt;
+    }
+
+    public function getDestinationId() {
+        return $this->destination_id;
+    }
+
+
+}
+
+
+
+
+
+?>
